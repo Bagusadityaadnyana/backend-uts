2b7e09e973afb68b31388742474eb45f80fbf043
Bagusadityaadnyana <Bagusadityaadnyana@gmail.com>
2023-11-21 21:41:33 +0800

    p

--- main.ts
+++ main.ts
@@ -1,39 +1,39 @@
-import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
-import "https://deno.land/x/dotenv/load.ts";
-import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import data from "./data.json" assert { type: "json" };
-
+import { SmtpClient } from 'https://deno.land/x/smtp/mod.ts';
+import 'https://deno.land/x/dotenv/load.ts';
+import { Hono } from 'https://deno.land/x/hono@v3.4.1/mod.ts';
+import data from './data.json' assert { type: 'json' };
 
 const { PASSWORD } = Deno.env.toObject();
 const client = new SmtpClient();
 const connectConfig: any = {
-  hostname: "smtp.gmail.com",
+  hostname: 'smtp.gmail.com',
   port: 465,
-  username: "bagusadityaadnyana@gmail.com",
+  username: 'bagusadityaadnyana@gmail.com',
   password: PASSWORD,
 };
+console.log(connectConfig);
 await client.connectTLS(connectConfig);
 const app = new Hono();
 
-app.get("/", (c) => c.text("Welcome to dinosaur API!"));
+app.get('/', (c) => c.text('Welcome to dinosaur API!'));
 
-app.get("/api/", async (c) => {
+app.get('/api/', async (c) => {
   await client.send({
-    from: "bagusadityaadnyana@gmail.com",
-    to: "jimmyeatcrab@gmail.com",
-    subject: "Welcome!",
-    content: "Hi from Vuelancer!",
+    from: 'bagusadityaadnyana@gmail.com',
+    to: 'jimmyeatcrab@gmail.com',
+    subject: 'Welcome!',
+    content: 'Hi from Vuelancer!',
   });
   await client.close();
 });
 
-app.get("/api/:dinosaur", (c) => {
-  const dinosaur = c.req.param("dinosaur").toLowerCase();
+app.get('/api/:dinosaur', (c) => {
+  const dinosaur = c.req.param('dinosaur').toLowerCase();
   const found = data.find((item) => item.name.toLowerCase() === dinosaur);
   if (found) {
     return c.json(found);
   } else {
-    return c.text("No dinosaurs found.");
+    return c.text('No dinosaurs found.');
   }
 });
 
